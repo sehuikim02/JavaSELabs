6ea7cf6167fd0834228206891bfd16b622d4c6e8
sehuikim02 <lulla1613@gmail.com>
2025-08-06 17:24:44 +0900

    Encapsulation 개념, 배열 연습하기

--- src/workshop/person/control/PersonManager.java
+++ src/workshop/person/control/PersonManager.java
@@ -1,10 +1,15 @@
 package workshop.person.control;
 
 public class PersonManager {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PersonManager personMgr = new PersonManager();
+		
+		personMgr.printTitle("인물정보 조회시스템");
 
+	}
+	
+	public void printTitle(String title) {
+		System.out.println("@@@@ " + title + " @@@@");
 	}
 
 }
